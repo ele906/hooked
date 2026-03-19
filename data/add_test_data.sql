b7694bd4bc8825d996927c1572e9fe5417a6cd2c
ele906 <eleanorliu8@gmail.com>
2026-03-19 14:32:22 -0400

    test add data

---
+++ data/add_test_data.sql
@@ -0,0 +1,8 @@
+-- Insert a test user
+INSERT INTO users (email) VALUES ('testuser');
+
+-- Insert a test artist
+INSERT INTO artists (artist_name) VALUES ('Test Artist');
+
+-- Insert a test song
+INSERT INTO songs (song_name, preview_mp3_url) VALUES ('Test Song', 'www.google.com');
