6a2b78bd7a31657d49df2410e1a4130986c6fb28
ele906 <eleanorliu8@gmail.com>
2026-03-19 14:35:52 -0400

    data tutorial

--- data/add_test_data.sql
+++ data/add_test_data.sql
@@ -1,8 +1,11 @@
 -- Insert a test user
-INSERT INTO users (email) VALUES ('testuser');
+INSERT INTO users (email) VALUES ('user0');
+INSERT INTO users (email) VALUES ('user1');
 
 -- Insert a test artist
-INSERT INTO artists (artist_name) VALUES ('Test Artist');
+INSERT INTO artists (artist_name) VALUES ('Artist 0');
+INSERT INTO artists (artist_name) VALUES ('Artist 1');
 
 -- Insert a test song
-INSERT INTO songs (song_name, preview_mp3_url) VALUES ('Test Song', 'www.google.com');
+INSERT INTO songs (song_name, preview_mp3_url) VALUES ('Test Song 0', 'www.google.com');
+INSERT INTO songs (song_name, preview_mp3_url) VALUES ('Test Song 1', 'www.youtube.com');
