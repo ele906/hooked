5163be96318f1e3615564ed859c235a9808c6bd9
Stephen Gilton <sg8409@princeton.edu>
2026-03-21 21:23:56 -0400

    made backend folder, added app.py to it, also fixed db.py to actually return the db connection

---
+++ backend/app.py
@@ -0,0 +1,84 @@
+from flask import Flask, jsonify, request
+from data.db import get_db
+
+app = Flask(__name__)
+
+
+def sql_cmd(command, params=(), fetch=False):
+    conn = get_db()
+    cur = conn.cursor()
+    
+    cur.execute(command, params)
+    conn.commit()
+    
+    result = cur.fetchall() if fetch else None
+    cur.close()
+    conn.close()
+    return result
+
+@app.route("/api/songs/action", methods=["POST"])
+def store_interaction():
+    data = request.get_json()
+
+    sql_cmd(
+        "INSERT INTO interactions (user_id, song_id, type) VALUES (%s, %s, %s);",
+        (data["user_id"], data["song_id"], data["action"])
+    )
+    return jsonify({"status": "ok"}), 201
+
+@app.route("/api/songs/liked", methods=["GET"])
+def get_liked_songs():
+    user_id = request.args.get("user_id")
+
+    rows = sql_cmd("""
+        SELECT s.song_id, s.song_name, s.song_image_url, s.preview_mp3_url,
+               a.artist_name, l.created_at
+        FROM liked l
+        JOIN songs s ON l.song_id = s.song_id
+        JOIN song_artists sa ON s.song_id = sa.song_id
+        JOIN artists a ON sa.artist_id = a.artist_id
+        WHERE l.user_id = %s
+        ORDER BY l.created_at DESC;
+        """, 
+        (user_id, ),
+        fetch = True
+    )
+
+    return jsonify([{
+            "song_id":          r[0],
+            "song_name":        r[1],
+            "song_image_url":   r[2],
+            "preview_mp3_url":  r[3],
+            "artist_name":      r[4],
+            "liked_at":         r[5]
+        } for r in rows])
+
+
+@app.route("/api/songs/next", methods=["GET"])
+def next_song():
+    user_id = request.args.get("user_id")
+    rows = sql_cmd("""
+            SELECT s.song_id, s.song_name, s.song_image_url, s.preview_mp3_url,
+                a.artist_name
+            FROM songs s
+            JOIN song_artists sa ON s.song_id = sa.song_id
+            JOIN artists a ON sa.artist_id = a.artist_id
+            WHERE s.song_id NOT IN (
+                SELECT song_id FROM liked WHERE user_id = %s
+                UNION
+                SELECT song_id FROM disliked WHERE user_id = %s
+            )
+            LIMIT 1;
+        """, (user_id, user_id), fetch=True)
+
+    if not rows:
+        return jsonify({"message": "no more songs"}), 404
+
+    r = rows[0]
+    return jsonify({
+        "song_id":          r[0],
+        "song_name":        r[1],
+        "song_image_url":   r[2],
+        "preview_mp3_url":  r[3],
+        "artist_name":      r[4]
+    })
--- data/db.py
+++ data/db.py
@@ -8,9 +8,8 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 def get_db():
     try:
-        with psycopg.connect(DATABASE_URL) as conn:
-            with conn.cursor() as cur:
-                print("Successfully connected to Hooked Database!")
+        conn = psycopg.connect(DATABASE_URL)
+        return conn
     except Exception as e:
         print(f"Connection failed: {e}")
         sys.exit(1)
