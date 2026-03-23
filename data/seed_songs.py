aff43740049d688daada343b9b5234a550c1617b
Lucille Rizo Patron <lr6019@princeton.edu>
2026-03-23 15:13:33 -0400

    setting up audio previews

--- data/seed_songs.py
+++ data/seed_songs.py
@@ -3,14 +3,28 @@ import time
 from db import get_db
 import sys
 
-if len(sys.argv) < 2:
-    print("Not enough arguments")
-    sys.exit(1)
-songs_to_add = sys.argv[1:]
+
+# if len(sys.argv) < 2:
+#   print("Not enough arguments")
+#   sys.exit(1)
+#songs_to_add = sys.argv[1:]
+
+SONGS_TEST = [
+    "The Weeknd Blinding Lights",
+    "The Weeknd Starboy",
+    "The Weeknd Save Your Tears",
+    "The Weeknd Can't Feel My Face",
+    "The Weeknd The Hills",
+    "The Weeknd Earned It",
+    "The Weeknd Often",
+    "The Weeknd Heartless",
+    "The Weeknd Die For You",
+    "The Weeknd In Your Eyes",
+]
 
 conn = get_db()
 with conn.cursor() as cur:
-    for query in songs_to_add:
+    for query in SONGS_TEST:
         resp = requests.get("https://itunes.apple.com/search", params={
             "term": query,
             "media": "music",
@@ -19,8 +33,14 @@ with conn.cursor() as cur:
         track = resp.json()["results"][0]
         print(f"Adding {track['trackName']} by {track['artistName']} to the database."
             )
-        cur.execute("INSERT INTO artists (artist_name) VALUES (%s) RETURNING artist_id",
-                    (track["artistName"],))
+        cur.execute(
+            "INSERT INTO artists (artist_name) VALUES (%s) ON CONFLICT (artist_name) DO NOTHING;",
+            (track["artistName"],)
+        )
+        cur.execute(
+            "SELECT artist_id FROM artists WHERE artist_name = %s",
+            (track["artistName"],)
+        )
         artist_id = cur.fetchone()[0]
         cur.execute(
             "INSERT INTO songs (song_name, preview_mp3_url, song_image_url, duration_ms) VALUES (%s, %s, %s, %s) RETURNING song_id",
