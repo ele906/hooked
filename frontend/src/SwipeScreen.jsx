f733b01dec5dfa9270e2564cc154d9a155d3261b
Lucille Rizo Patron <lr6019@princeton.edu>
2026-03-29 02:57:25 -0400

    any changes i forgot to save

--- frontend/src/SwipeScreen.jsx
+++ frontend/src/SwipeScreen.jsx
@@ -2,6 +2,7 @@
 // SwipeScreen.jsx
 // Swipe Interface for Hooked
 // Author: Lucille Rizo Patron
+// Contributors: Eleanor Liu, Sadat Ahmed
 // -----------------------------------------------------------------------
 
 import React from 'react'
