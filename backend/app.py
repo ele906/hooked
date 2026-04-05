cda5052699027e978b78a0835fd3d3a03b3c4f1c
derekgeng15 <derekgeng@princeton.edu>
2026-04-05 17:25:56 -0400

    added port

--- backend/app.py
+++ backend/app.py
@@ -370,4 +370,4 @@ def check_password():
     return flask.jsonify({'logged_in': False, 'error': 'Wrong password'}), 401
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
